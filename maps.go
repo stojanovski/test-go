f0a0aa895955d619805fd4b22fdc8bbb6d719485
Igor Stojanovski <stojanovski@users.noreply.github.com>
2018-03-02 14:35:25 -0800

    adding some go code and some testing code

---
+++ maps.go
@@ -0,0 +1,33 @@
+import "fmt"
+
+func main() {
+    m := make(map[string]int)
+
+    m["Answer"] = 42
+    fmt.Println("The value:", m["Answer"])
+
+    m["Answer"] = 48
+    fmt.Println("The value:", m["Answer"])
+
+    delete(m, "Answer")
+    fmt.Println("The value:", m["Answer"])
+
+    v, ok := m["Answer"]
+    fmt.Println("The value:", v, "Present?", ok)
+
+    n := make(map[string]int)
+    i, j := n["one"]
+    fmt.Println(n, i, j)
+    n["one"] = 1
+    i, j = n["one"]
+    fmt.Println(n, i, j)
+
+    o := make(map[int]string)
+    //o[0] = "zero"
+    if o == nil {
+        fmt.Println("nil")
+    }
+    fmt.Println(o[0], "AA")
+    fmt.Println(m["fifty"])
+}
+
---
+++ maps_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestSomething(t *testing.T) {
+    //t.Error("FAIL")
+}
+
+func TestSomethingElse(t *testing.T) {
+    // test stuff here...
+}
+
+
